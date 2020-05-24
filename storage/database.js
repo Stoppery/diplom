cb76d47473c1af6096ff829ba78f2994df725d3b
foxyuiop <nika1294@gmail.com>
2020-05-24 22:37:14 +0300

    Test

--- storage/database.js
+++
@@ -1,9 +0,0 @@
-const Client = require('pg-native');
-
-module.exports.database = {
-    dbConnect: function (PGHOST, PGUSER, PGDATABASE, PGPASSWORD, PGPORT) {
-        let client = new Client();
-        client.connectSync(`postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}:${PGPORT}/${PGDATABASE}`);
-        return client;
-    }
-};
